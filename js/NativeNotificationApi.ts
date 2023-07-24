d035ee00864e6b28d1bc477a1315c2693851d150
Devin <devthedevel@gmail.com>
2023-07-24 13:26:41 -0230

    Create NativeNotificationApi.ts

---
+++ js/NativeNotificationApi.ts
@@ -0,0 +1,12 @@
+import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  configure(clientId: string, userId: string, hashedUserId?: string): void;
+
+  requestNotificationPermission(): void;
+}
+
+export default TurboModuleRegistry.get<Spec>(
+  'RTNNotificationApi',
+) as Spec | null;
